79d2b6f50dada326f31c4ac27e492eca58630a60
Amaury Fouquet-Lapar <afouquetlapar@gmail.com>
2019-10-23 21:22:12 +0200

    Add the CSV files reading

--- Exercice1/main.py
+++ Exercice1/main.py
@@ -5,18 +5,47 @@ Created on Tue Oct 22 16:35:15 2019
 
 @author: amaury
 """
-
+import csv
+import jobType
+import jobOffer
 def main():
     """Main function :
     No input
     Output : the desired tab"""
 
-    #Read the CSV files
+    # Read the job offers CSV file and fill a list with it
+    jobOfferList = []
+    with open('/home/amaury/Bureau/Test Technique - QA Engineer/appendix/technical-test-jobs.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        header = True
+        for row in csv_reader:
+            if header:
+                print(f'Job offers column names are {", ".join(row)}')
+                header = False
+            else:
+                job = jobOffer.JobOffer(row[0], row[1], row[2], row[3], row[4])
+                jobOfferList += [job]
+        print("Number of imported offers : " + str(len(jobOfferList)) + "\n")
     
-    #Create two lists of objects
+    # Read the job types CSV file and fill a list with it
+    jobTypeList = []
+    with open('/home/amaury/Bureau/Test Technique - QA Engineer/appendix/technical-test-professions.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        header = True
+        for row in csv_reader:
+            if header:
+                print(f'Job types column names are {", ".join(row)}')
+                header = False
+            else:
+                job = jobType.JobType(row[0], row[1], row[2])
+                jobTypeList += [job]
+        print("Number of imported types : " + str(len(jobTypeList)))
     
     #Create an empty output tab with all the lines and columns
     
     #Update the output tab
     
     #Show the tab
+    
+
+main()  
